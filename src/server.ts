6fe434889c34bc6394bcc0badd408f85fd796182
mariop06 <mariopaulobunga.com>
2024-08-17 21:07:59 +0100

    criado o prisma migrate

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,22 @@
 import cors from "@fastify/cors";
 import fastify from "fastify";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+
+
+require('dotenv').config();
 
 const app = fastify();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
 app.register(cors,{
     origin:"*"
 })
 
 
+const port= process.env.SERVER_PORT;
 
-
-
-app.listen({port:8080}).then(()=>{
-    console.log('Servidor Rodando...');
+app.listen({port:Number(port)}).then(()=>{
+    console.log(`Servidor Rodando...`);
 })
